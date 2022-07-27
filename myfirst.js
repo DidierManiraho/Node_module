cc937dbee7bd6e8b0567e2a94b56d18cbaddad58
Didier Maniraho <maniraho.didier2190@gmail.com>
2022-07-27 12:23:36 -0400

    adding the first npm package uppercase

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var uc = require('upper-case');
 
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('Hello World!');
+    res.end(uc.upperCase('Hello World!'));
 }).listen(8000);
